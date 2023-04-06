749f5fbff68e40ec3e04aed44226f5afad5f6236
Gonzalo Ruiz <rgon@rgon.es>
2023-04-06 12:38:51 +0200

    style: reindent lines

--- src/TicketForm.svelte
+++ src/TicketForm.svelte
@@ -1,26 +1,26 @@
 <script>
     import { createEventDispatcher } from 'svelte';
-  
+    
     const dispatch = createEventDispatcher();
-  
+    
     let price = '';
-  
+    
     function handleSubmit() {
-      const ticket = {
-        price: parseFloat(price),
-        id: Math.floor(Math.random() * 1000)
-      };
-  
-      dispatch('addTicket', ticket);
-      price = '';
+        const ticket = {
+            price: parseFloat(price),
+            id: Math.floor(Math.random() * 1000)
+        };
+        
+        dispatch('addTicket', ticket);
+        price = '';
     }
-  </script>
-  
-  <form on:submit|preventDefault={handleSubmit}>
-    <label>
-      Precio:
-      <input type="number" bind:value={price} />
-    </label>
+</script>
 
+<form on:submit|preventDefault={handleSubmit}>
+    <label>
+        Precio:
+        <input type="number" bind:value={price} />
+    </label>
+    
     <button type="submit">Añadir ticket</button>
-  </form>
+</form>
